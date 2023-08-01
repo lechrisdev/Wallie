6ef7b9fb4a6af44210dac7a240b6cb88d9bcc9c3
LeChris <bk2mua@gmail.com>
2023-08-01 14:01:33 +0300

    Extract SubView (ImagesGridView) from MainView, which shows pictures of selected category (to reuse it in SearchScreenView).

---
+++ Wallie/ResultScreenView/ResultScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  ResultScreenView.swift
+//  Wallie
+//
+//  Created by Le Chris on 01.08.2023.
+//
+
+import SwiftUI
+
+struct ResultScreenView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ResultScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultScreenView()
+    }
+}
